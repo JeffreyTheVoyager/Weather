9277b44cce6e239055621035f0528f5fb0640619
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-27 13:44:00 -0500

    Render view folder and add view as express static

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 require("dotenv").config()
 
+// Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// View Engine
+const viewPath = path.join(__dirname, '/view')
+app.set('views', viewPath)
+app.set('view engine', 'pug')
+app.use(express.static(viewPath))
+
+
 app.get('/', (req, res)=>{
-    res.json({
-        message: "SUCCESS",
-        statusCode: 201
-    })
+    res.render('index')
 })
 
 app.listen(process.env.PORT, ()=>{
-    console.log("Server Successful")
+    console.log("Server Successful");
 })
