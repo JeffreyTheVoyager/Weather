1f88fc67195f2fafad43349d4afe584f6cbf29b4
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-27 20:59:46 -0500

    Render date and icon

--- routers/home/home.js
+++ routers/home/home.js
@@ -2,12 +2,15 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const url = require('url')
+const dateTime = require('date-and-time')
 
 router.get('/', (req, res) =>{
     res.render('index',{
         city: req.query.city,
         temp: req.query.temp,
-        description: req.query.description
+        description: req.query.description,
+        date: req.query.date,
+        icon: req.query.icon
     })
 })
 
@@ -19,26 +22,30 @@ router.post('/', (req, res)=>{
 
                 const data = response.data;
                 let {name:city, main:{temp}} = data;
-                const {description} = data.weather[0]
+                const {description, icon} = data.weather[0]
                 
                 // Convert Kelvin to Fahrenheit
                 temp = Math.round((temp - 273.15) * 9/5 + 32)
+
+                // Date
+                const now = new Date()
+                const pattern = dateTime.compile('DD MMM')
+                const date = dateTime.format(now, pattern)
 
                 res.redirect(url.format({
                     pathname: '/',
                     query:{
                         city,
                         temp,
-                        description
+                        description,
+                        date,
+                        icon
                     }
                 }))
             })
 
     } catch (error) {
-       res.status(400).json({
-            msg: "Bad Request",
-            error
-       })
+       res.status(400)  
     }
 })
 
